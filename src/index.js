17284bc431e194b0639786d47a973fa7a8da81f4
Martyna Bors <martynabors@gmail.com>
2019-01-03 21:58:27 +0100

    minor structure cleanup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import React, { Component } from 'react';
+import { Route, Router } from 'react-router';
+import { Provider } from 'react-redux';
+import { configureStore } from './store';
+import { createBrowserHistory } from 'history';
+import  Main  from './containers/App/index';
+
+
+const history = createBrowserHistory();
+const store = configureStore();
+
+export default class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <Router history={history}>
+            <Route path="/" component={Main} />
+        </Router >
+      </Provider>
+    );
+  }
+}
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
